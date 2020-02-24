3be05a5754ab4253d0dbba505946b98026ac9346
Howard McLauchlan <work@hmclauchlan.com>
2020-02-23 23:14:13 -0800

    hacky thing to take dims

--- i3bgwin.c
+++ i3bgwin.c
@@ -24,6 +24,24 @@ int main(int argc, char *argv[]) {
   }
 
   int screen = DefaultScreen(display);
+
+  // parse for screen args
+  int dims[4] = {0, 0,DisplayWidth(display, screen), DisplayHeight(display, screen)};
+  fprintf(stderr,"ree %d", dims[0]);
+  fprintf(stderr,"ree %d", dims[1]);
+  fprintf(stderr,"ree %d", dims[2]);
+  fprintf(stderr,"ree %d", dims[3]);
+
+  int arg_start = 0;
+  for (;;++arg_start) {
+      if (argv[arg_start + 1] && strcmp(argv[arg_start + 1], "--") == 0) {
+          break;
+      }
+      dims[arg_start] = atoi(argv[arg_start + 1]);
+  }
+  ++arg_start;
+  fprintf(stderr, "args_start %d", arg_start);
+
   XVisualInfo vinfo;
   XMatchVisualInfo(display, screen, 32, TrueColor, &vinfo);
 
@@ -36,17 +54,25 @@ int main(int argc, char *argv[]) {
   attrs.background_pixel = 0;
   attrs.override_redirect = True;
 
+  fprintf(stderr, "%d", DisplayHeight(display, screen));
   Window window = XCreateWindow(
-      display, RootWindow(display, screen), 0, 0, DisplayWidth(display, screen),
-      DisplayHeight(display, screen), 0, vinfo.depth, InputOutput, vinfo.visual,
+      display, RootWindow(display, screen), dims[0], dims[1], dims[2], dims[3],  0, vinfo.depth, InputOutput, vinfo.visual,
       CWEventMask | CWColormap | CWBorderPixel | NoEventMask | CWBackPixel | CWOverrideRedirect,
       &attrs);
+      /*
+  Window window = XCreateWindow(
+      display, RootWindow(display, screen), 0, 0, 4000, 2560,  0, vinfo.depth, InputOutput, vinfo.visual,
+      CWEventMask | CWColormap | CWBorderPixel | NoEventMask | CWBackPixel | CWOverrideRedirect,
+      &attrs);
+  */
 
   Atom wmDeleteMessage = XInternAtom(display, "WM_DELETE_WINDOW", False);
   XSetWMProtocols(display, window, &wmDeleteMessage, 1);
 
   Atom netWmWindowType = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
   Atom netWmWindowTypeDesktop = XInternAtom(display, "_NET_WM_WINDOW_TYPE_DESKTOP", False);
+
+  //Atom netWmWindowTypeFullScreenMonitors = XInternAtom(display, "_NET_WM_FULLSCREEN_MONITORS", False);
   XChangeProperty(display, window, netWmWindowType, XA_ATOM, 32, PropModeReplace,
                   (unsigned char *)&netWmWindowTypeDesktop, 1);
 
@@ -66,13 +92,15 @@ int main(int argc, char *argv[]) {
     char window_id[64];
     sprintf(window_id, "%lu", window);
 
-    char **child_argv = calloc(argc, sizeof(char *));
-    for (int i = 0; i < argc; ++i) {
+    char **child_argv = calloc(argc - arg_start, sizeof(char *));
+    for (int i = arg_start; i < argc; ++i) {
       if (argv[i + 1] && strcmp(argv[i + 1], "{windowid}") == 0) {
-        child_argv[i] = window_id;
+        child_argv[i - arg_start] = window_id;
       } else {
-        child_argv[i] = argv[i + 1];
+        child_argv[i - arg_start] = argv[i + 1];
+        fprintf(stderr, "ree %d", i - arg_start);
       }
+      fprintf(stderr,"args %s\n", child_argv[i - arg_start]);
     }
 
     execvp(child_argv[0], child_argv);
